3a80c7c0e550bd74cf8f9b85d1c50d44af4293ce
Ania Wawerek <wawerekanna@gmail.com>
2022-04-07 17:52:22 +0200

    11.5 Add Button and container

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,12 +1,15 @@
 import styles from './SearchForm.module.scss';
+import TextInput from '../TextInput/TextInput';
+import Button from '../Button/Button';
 
 const SearchForm = () => {
     return (
-        <form className={styles.SearchForm}>
-        <input type="text" className={styles.input} />
-        <button className={styles.button}>Search</button>
-    </form>
+        <form className={styles.searchForm}>
+            <TextInput placeholder="Search..." />
+            <Button>
+                <span className="fa fa-search" />
+            </Button>
+        </form>
     );
   };
-
-  export default SearchForm;
+export default SearchForm;
