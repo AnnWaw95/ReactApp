0a1d711b9f793ef2e74dc672039dcb42acbb86c0
Ania Wawerek <wawerekanna@gmail.com>
2022-04-27 08:05:19 +0200

    14.1 Add favorite about and not found

---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+import PageTitle from "../PageTitle/PageTitle";
+
+const About = () => {
+    return (
+      <div>
+        <PageTitle>ABOUT</PageTitle>
+        <p> Do you want to learn something more about ReactApp? </p>
+      </div>
+    );
+  };
+
+export default About;
---
+++ src/components/Favorite/Favorite.js
@@ -0,0 +1,12 @@
+import PageTitle from "../PageTitle/PageTitle";
+
+const Favorite = () => {
+    return (
+      <div>
+        <PageTitle>FAVORITE</PageTitle>
+        <p> Here is your favorite ReactApp </p>
+      </div>
+    );
+  };
+
+export default Favorite;
