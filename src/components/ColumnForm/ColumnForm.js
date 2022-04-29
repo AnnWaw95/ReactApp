3ac599c07335a32470a3887e97b3faaedf4716d8
Ania Wawerek <wawerekanna@gmail.com>
2022-04-29 09:12:23 +0200

    14.3 Add ListForm

--- src/components/ColumnForm/ColumnForm.js
+++ src/components/ColumnForm/ColumnForm.js
@@ -16,7 +16,7 @@ const ColumnForm = props => {
     
     const handleSubmit = e => {
         e.preventDefault();
-        dispatch(addColumn({ title, icon }));
+        dispatch(addColumn({title, icon, listId: props.listId}));
         setTitle('');
         setIcon('');
      };
