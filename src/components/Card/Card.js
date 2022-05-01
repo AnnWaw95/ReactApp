5b9d38c9d0dff738692a11368c8d24f9f85025c4
Ania Wawerek <wawerekanna@gmail.com>
2022-05-01 11:35:46 +0200

    14.5 Add remove cards and subreducers

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,21 +1,28 @@
 import styles from './Card.module.scss';
 import clsx from 'clsx';
 import { useDispatch } from 'react-redux';
-import { toggleFavoriteCart } from '../../redux/store';
+import { toggleFavoriteCart } from '../../redux/cardsRedux';
+import { removeCard } from '../../redux/cardsRedux';
 
 const Card = props => {
     
     const dispatch = useDispatch;
-    const cardId = props.id
 
-    const classChange = e => {
-        e.preventDefault()
-        dispatch(toggleFavoriteCart(cardId))};
+    const classChange = (cardId) => {
+        dispatch(toggleFavoriteCart(cardId))
+    };
+
+    const removedCard = (cardId) => {
+        dispatch(removeCard(cardId))
+    };
 
     return (
         <li className={styles.card}>{props.title}
-            <button type="button" onClick={() => classChange(props.id, props.isFavorite)} className={clsx(styles.button, props.isFavorite && styles.active) }>
+            <button type="button" onChange={() => classChange(props.id, props.isFavorite)} className={clsx(styles.button, props.isFavorite && styles.active) }>
                 <span className='fa fa-star-o' />
+            </button>
+            <button type="button" onChange={() => removedCard(props.id)} className={styles.button}>
+                <span className='fa fa-trash' />
             </button>
         </li>
     );
