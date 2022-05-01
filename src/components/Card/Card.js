4a8a55e9d86fadfdf3010da126e504f8483df7d6
Ania Wawerek <wawerekanna@gmail.com>
2022-05-01 15:02:11 +0200

    14 Add right favorite and remove button

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,24 +6,29 @@ import { removeCard } from '../../redux/cardsRedux';
 
 const Card = props => {
     
-    const dispatch = useDispatch;
+    const dispatch = useDispatch();
+    const cardId = props.id
 
-    const classChange = (cardId) => {
-        dispatch(toggleFavoriteCart(cardId))
-    };
+    const classChange = e => {
+        e.preventDefault()
+        dispatch(toggleFavoriteCart( cardId ))
+      }
 
-    const removedCard = (cardId) => {
+    const removedCard = e => {
+        e.preventDefault()
         dispatch(removeCard(cardId))
     };
 
     return (
         <li className={styles.card}>{props.title}
-            <button type="button" onChange={() => classChange(props.id, props.isFavorite)} className={clsx(styles.button, props.isFavorite && styles.active) }>
+            <div>
+            <button onClick={classChange} className={clsx(styles.button, props.isFavorite && styles.isActive)}>
                 <span className='fa fa-star-o' />
             </button>
-            <button type="button" onChange={() => removedCard(props.id)} className={styles.button}>
+            <button onClick={removedCard} className={styles.button}>
                 <span className='fa fa-trash' />
             </button>
+            </div>
         </li>
     );
 };
